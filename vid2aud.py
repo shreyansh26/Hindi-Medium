505d9cce359aabd94561b75d5333cb5c0d339fab
Shreyansh <shreyansh.pettswood@gmail.com>
2018-01-09 19:47:09 +0530

    Initial version

---
+++ vid2aud.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import youtube_dl
+import glob, os
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+
+for file in glob.glob("./*.wav"):
+    os.rename(file, 'audio.wav')
