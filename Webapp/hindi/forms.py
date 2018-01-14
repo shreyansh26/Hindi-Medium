ffced7fd230aa327a5df4c4b105f0f940fd05a4b
Shreyansh <shreyansh.pettswood@gmail.com>
2018-01-14 12:41:51 +0530

    Small fixes

--- Webapp/hindi/forms.py
+++ Webapp/hindi/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class url_form(forms.Form):
-    url = forms.CharField(label='URL for the video', max_length=100)
+    url = forms.CharField(label='URL of the video', max_length=100)
     email = forms.EmailField(label='Enter your email')
