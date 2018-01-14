2510a53cdd81da9cf37c726d5eae123078d34610
Shreyansh <shreyansh.pettswood@gmail.com>
2018-01-15 02:13:49 +0530

    File

--- hindi/forms.py
+++ hindi/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class url_form(forms.Form):
     url = forms.CharField(label='URL of the video', max_length=100)
     email = forms.EmailField(label='Enter your email')
