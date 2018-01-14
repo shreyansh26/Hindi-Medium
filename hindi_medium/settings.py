f1ddc5fc7c62f707b3e271a1c74541a9feee725c
Shreyansh <shreyansh.pettswood@gmail.com>
2018-01-14 22:19:51 +0530

    Files

--- hindi_medium/settings.py
+++ hindi_medium/settings.py
@@ -105,12 +105,7 @@ EMAIL_HOST_PASSWORD = 'jrgumvpxnydnleeu'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-# import os.path
 Temp_path = os.path.realpath('.')
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 TEMPLATE_DIRS = (Temp_path + "/templates", )
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
